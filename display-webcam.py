e969f49aa4e0f63d1c375cedc90c85193b3db488
Amelia Laundy@pi <amelialaundy@gmail.com>
2017-04-05 00:29:39 +1200

    vertical concat

--- display-webcam.py
+++ display-webcam.py
@@ -15,7 +15,6 @@ while rval:
     cv2.imshow("preview", frame)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
-
     if key == 27: # exit on ESC
         break
     if key == 10:
@@ -24,14 +23,14 @@ while rval:
         #borderimg = cv2.rectangle(frame,(384,0),(510,128),(0,255,0),2)
         cv2.imwrite(name,frame)
         photonum = photonum + 1
-    if key == 1048675:
+    if key == 116: #t
         print 'restarting'
+        img1 = cv2.imread('./photos/1photo-1.png')
+        img2 = cv2.imread('./photos/1photo-2.png')
+        vis = np.concatenate((img1, img2), axis=0)
+        cv2.imwrite('./photos/out.png', vis)
         foldernum = foldernum +1
         photonum = 1
 
-img1 = cv2.imread('./photos/{0}photo-{1}.png'.format(foldernum,photonum))
-img2 = cv2.imread('./photos/{0}photo-{1}.png'.format(foldernum-1, photonum-1))
-vis = np.concatenate((img1, img2), axis=1)
-cv2.imwrite('/photos/out.png', vis)
-        
+       
 cv2.destroyWindow("preview") 
