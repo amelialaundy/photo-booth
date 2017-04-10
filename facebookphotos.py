780a23433d0d9f85c9d3608e85630e098bb3e4f6
amelialaundy <amelialaundy@gmail.com>
2017-04-10 21:13:20 +1200

    use config for album

--- facebookphotos.py
+++ facebookphotos.py
@@ -6,4 +6,4 @@ configFilePath = r'../keys.txt'
 configParser.read(configFilePath)
 
 graph = facebook.GraphAPI(access_token=configParser.get('facebook', 'access_token_photo_booth'), version='2.7')
-graph.put_photo(image=open("/media/pi/88DB-D77C/photos/vertical/Vertical-1491810510.jpg", 'rb'), album_path='151959131998755' + "/photos")
+graph.put_photo(image=open(configParser.get('facebook', 'test_photo'), 'rb'), album_path=configParser.get('facebook', 'test_album_id') + "/photos")
